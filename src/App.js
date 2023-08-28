20ffc017f8dd55bcb886863d25f33782676f73e7
shreyanshishukla <shreyashi1217@gmail.com>
2023-08-28 17:26:13 +0530

    i18next react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useTranslation } from "react-i18next";
+import LanguageSwitchers from "../src/LanguageSwitcher";
 
 function App() {
+  let { t } = useTranslation();
+  let name = "shreyanshi Shukla";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LanguageSwitchers />
+
+      {t("good_morning")}
+      <br />
+      {t("Welcome", { name: name })}
     </div>
   );
 }
